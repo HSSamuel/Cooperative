353ac7f77d8382ead7f3335b368848bc478ab42c
HSSamuel <smkmayomisamuel@gmail.com>
2026-05-10 00:22:59 +0100

    updates

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -102,7 +102,6 @@ router.post("/login", async (req, res) => {
 
     res.status(200).json({
       message: "Login successful",
-      token: token,
       user: {
         id: user._id,
         firstName: user.firstName,
